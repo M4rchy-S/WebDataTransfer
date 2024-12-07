68cbe3d1b4bb1bedd9b1b927675a5e97d07745a7
M4rchy-S <alex12345suslik@gmail.com>
2024-12-07 19:33:05 +0200

    Added download file function

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, current_app, send_from_directory
+import os
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return redirect(url_for("main_page"))
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return redirect(url_for("main_page"))
+
+@app.route("/data/<path:filename>", methods=["GET"])
+def download(filename):
+    downloads = os.path.join(current_app.root_path, "data")
+    #print(downloads + "/" + filename)
+    return send_from_directory(downloads, filename)
+
 
 @app.route("/")
 @app.route("/index.html")
 def main_page():
-    return render_template("index.html")
+    print(os.path.join(os.getcwd(), "WebFileTransfer", "data"))
+    file_names = os.listdir(os.path.join(os.getcwd(), "WebFileTransfer", "data"))
+    return render_template("index.html", names=file_names)
 
 
 if __name__ == '__main__':
